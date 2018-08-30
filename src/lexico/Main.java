e1f36d0d69e7fb18189a49c3ed4c8be059b1358f
Luis Enrique Galaviz Huerta <legh1997@gmail.com>
2018-08-30 09:10:38 -0600

    Clase Main
    Realizara la llamada al inicio de la aplicacion

---
+++ src/lexico/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Escuela: Centro Universitario de Ciencias Exactas e Ingenierias
+ * Nombre: Luis Enrique Galaviz Huerta
+ * Materia: Seminario de solucion de problemas de Traductores de lenguaje 2
+ * Trabajo: Analizador lexico
+ */
+
+package lexico;
+import lexico.visual.VentanaPrincipal;
+
+public class Main {	
+	public static void main(String[] args){	
+		new VentanaPrincipal();			
+	}
+}
