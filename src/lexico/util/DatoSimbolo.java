263a9019847540aa12cc8c7aa09dedbab1d252ae
Luis Enrique Galaviz Huerta <legh1997@gmail.com>
2018-08-30 09:22:31 -0600

    Interfaz DatoSimbolo
    Aqui se definen todas las constantes estaticas que contienen el datodel simbolo para identificar que tipo de tipo de simbolo fue leido  en las distintas etapas del compilador

---
+++ src/lexico/util/DatoSimbolo.java
@@ -0,0 +1,31 @@
+package lexico.util;
+
+public interface DatoSimbolo {
+    static final char ESPACIO = ' ';
+	static final char SUMA = '+';
+	static final char RESTA = '-';
+	static final char DIVISION = '/';
+	static final char MULTIPLICACION = '*';
+	static final char MAYOR = '>';
+	static final char MENOR = '<';
+	static final char OR = '|';
+	static final char AND = '&';
+	static final char NOT = '!';
+	static final char PUNTO = '.';
+	static final char PUNTO_Y_COMA = ';';
+	static final char COMA = ',';
+	static final char ABRIR_PARENTESIS = '(';
+	static final char CERRAR_PARENTESIS = ')';
+	static final char ABRIR_CORCHETE = '{';
+	static final char CERRAR_CORCHETE = '}';
+	static final char IGUAL = '=';
+	static final char PESO = '$';
+	static final char COMILLA= '"';
+	static final String INT = "int";
+	static final String FLOAT = "float";
+	static final String VOID = "void";
+	static final String IF = "if";
+	static final String WHILE = "while";
+	static final String RETURN = "return";
+	static final String ELSE = "else";
+}
