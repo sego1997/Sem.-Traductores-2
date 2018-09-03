b23cbc7c4deb0b83060f4d5572fb18f04208d1fd
Luis Enrique Galaviz Huerta <legh1997@gmail.com>
2018-09-03 07:02:52 -0600

    Interfaz ColumnaSimbolo
    Definicion de constantes estaticas para la declaracion de columnas en la tabla LR del analizador sintactico

---
+++ src/compilador/util/ColumnaSimbolo.java
@@ -0,0 +1,8 @@
+package compilador.util;
+
+public interface ColumnaSimbolo {
+	static final int COL_ID = 0;
+	static final int COL_MAS = 1;
+	static final int COL_PESO = 2;
+	static final int COL_REGLA = 3;
+}
