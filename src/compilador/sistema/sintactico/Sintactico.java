0b5d746d710c3b8687a03ee76465eeb5b33ed45a
Luis Enrique Galaviz Huerta <legh1997@gmail.com>
2018-09-18 09:55:26 -0600

    Clase Sintactico
    Tabla LR y reglas gramaticales cargadas a memoria e implementacion de la pila de objetos(Terminales,No Terminales , Acciones)

--- src/compilador/sistema/sintactico/Sintactico.java
+++ src/compilador/sistema/sintactico/Sintactico.java
@@ -1,127 +1,132 @@
 package compilador.sistema.sintactico;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Stack;
-
 import compilador.sistema.Compilador;
 import compilador.util.DatoSimbolo;
 import compilador.util.TipoSimbolo;
-import compilador.util.ColumnaSimbolo;
 
-public class Sintactico extends Thread implements TipoSimbolo,DatoSimbolo,ColumnaSimbolo{	   
-	private static final int TIEMPO = 500;
+public class Sintactico extends Thread implements TipoSimbolo,DatoSimbolo{	   
+	private static final int TIEMPO = 200;
 	private static final int POS_DATO = 0;
 	private static final int POS_TIPO = 1;
+	private static final int POS_COL_REGLA = 0;
+	private static final int POS_POPS_REGLA = 1;
+	private static final int POS_NOM_REGLA = 2;
+	private static final int MAX_FIL_TABLA = 95;
+	private static final int MAX_COL_TABLA = 46;
+	private static final int MAX_REGLAS = 53;
+	private static final String RUTA_TABLA = "C:\\Users\\kikito1997\\workspace\\Sem.-Traductores-2\\src\\compilador\\sistema\\sintactico\\archivos\\tabla.txt";
+	private static final String RUTA_REGLAS = "C:\\Users\\kikito1997\\workspace\\Sem.-Traductores-2\\src\\compilador\\sistema\\sintactico\\archivos\\reglas.txt";
 	
-	private final Compilador compilador;
-	
+	private final Compilador compilador;	
 	private int fil;
 	private int col;
 	private boolean continuar;
     private int simboloActual;    
-    private Stack<Simbolo> pilaSintactica;
-    
-    private static final int[] reglas = {6,2};
-    private static final int[][] tablaLR = {
-									{2,0,0,1},
-									{0,0,-1,0},
-									{0,3,-3,0},
-									{2,0,0,4},
-									{0,0,-2,0}
-		};
+    private Stack<Simbolo> pilaSintactica;  
+    public static String[] reglas;
+    public static int[][] tablaLR;
     
     public Sintactico(Compilador compilador){
         this.compilador = compilador;        
     }
     
     @Override
-    public void run(){
-    	pausar();
+    public void run(){    	    	  		
     	String entrada;	
     	continuar = true;
+    	simboloActual = 0;
         pilaSintactica = new Stack<Simbolo>();    	
     	pilaSintactica.push(new Terminal(PESO+""));
-    	pilaSintactica.push(new Accion(COL_ID+""));
-    	compilador.dameEscribano().imprimirDatosSintacticos("	Datos sintacticos \n"+mostrarPila());
-        while(compilador.puedeContinuar() && continuar){
-        	try {	        	
-				sleep(TIEMPO);										
-				fil = Integer.valueOf(pilaSintactica.peek().dameDato());		
-				if(compilador.dameSimbolos().size()>simboloActual){					
-					String[] simbolos = compilador.dameSimbolos().get(simboloActual).split(SEPARADOR);
-					entrada = simbolos[POS_DATO];
-					col =  Integer.valueOf(simbolos[POS_TIPO]);	
-					castearTipoSimbolo();
-				}else{
-					col = COL_PESO;
-					entrada = PESO+"";
-				}
-	        	int accion = tablaLR[fil][col];        	               		        	
-	        	String datos = "\n	Entrada "+entrada+"\n	Accion >> d"+accion+"\n";
-	        	compilador.dameEscribano().imprimirDatosSintacticos(datos);	        	
-	        	if(accion==-1) {
-	        		continuar = false;
-	        	}else if(accion<0){
-	        		reduccion(accion);
-	        	}else if(accion>0){	        		
-	        		pilaSintactica.push(new Terminal(entrada));
-	        		pilaSintactica.push(new Accion(accion+""));
-	        		compilador.dameEscribano().imprimirDatosSintacticos(mostrarPila());
-	        	}else{
-	        		if(accion==0){
-	        			compilador.puedeContinuar(false);
-	        		}
-	        	}
-			} catch (InterruptedException e) {}
-        	if(compilador.dameSimbolos().size()>simboloActual && compilador.dameLexico().puedeContinuar()){
-        		pausar();
-        	}
-        	simboloActual++;
+    	pilaSintactica.push(new Accion(ES_IDENTIFICADOR+""));
+    	cargarCompilador();    	
+    	compilador.dameEscribano().imprimirDatosSintacticos("	  Datos sintacticos \n");    	
+        while(compilador.puedeContinuar() && continuar){ 
+        	if(compilador.dameLexico().puedeContinuar()) {
+	        	pausar();
+	        }
+			compilador.dameEscribano().imprimirDatosSintacticos(mostrarPila());
+			fil = Integer.valueOf(pilaSintactica.peek().dameDato());		
+			if(compilador.dameSimbolos().size()>simboloActual){					
+				String[] simbolos = compilador.dameSimbolos().get(simboloActual).split(SEPARADOR);
+				entrada = simbolos[POS_DATO];
+				col =  Integer.valueOf(simbolos[POS_TIPO]);	
+			}else{
+				col = ES_PESO;
+				entrada = PESO+"";
+			}
+	        int accion = tablaLR[fil][col];        	
+	        if(accion==-1) {
+	        	continuar = false;
+	        }else if(accion<0){
+	        	reduccion(accion+2);
+	        }else if(accion>0){	
+	        	simboloActual++;
+	        	pilaSintactica.push(new Terminal(entrada));
+	        	pilaSintactica.push(new Accion(accion+""));
+	        }else if(accion==0){
+	        	compilador.puedeContinuar(false);
+	        }	        
         }
-        compilador.dameEscribano().imprimirDatosSintacticos("\n	Finalizado ...");
-    }
-    
-    private void reduccion(int tp){
-    	tp = tp*-1;
-    	int tamaPops = reglas[tp-COL_PESO];
+        compilador.dameEscribano().imprimirDatosSintacticos("\n\n     Finalizado ...");
+    }       
+   
+    private void reduccion(int idRegla){
+    	idRegla *= -1;
+    	String[] regla = reglas[idRegla].split(SEPARADOR);
+    	int tamaPops = Integer.valueOf(regla[POS_POPS_REGLA]); 
     	for(int i=0; i<tamaPops; i++){
     		pilaSintactica.pop();
     	}
-    	fil = Integer.valueOf(pilaSintactica.peek().dato);
-    	col = COL_REGLA;
-    	pilaSintactica.push(new NoTerminal("E"));
+    	fil = Integer.valueOf(pilaSintactica.peek().dameDato());
+    	col = Integer.valueOf(regla[POS_COL_REGLA]);     	
+    	pilaSintactica.push(new NoTerminal(regla[POS_NOM_REGLA]));    	
     	pilaSintactica.push(new Accion(tablaLR[fil][col]+""));
-    	compilador.dameEscribano().imprimirDatosSintacticos(mostrarPila());
     }
     
     private String mostrarPila(){
-    	String datosPila = "\n	Pila:\n	";
-    	int tamaPila = pilaSintactica.size();
+    	String datosPila = "\n     ";
+    	int tamaPila = pilaSintactica.size();    	
     	for(int i=0; i<tamaPila; i++){
-    		datosPila += pilaSintactica.get(i).dameDato();
+    		datosPila += pilaSintactica.get(i).dameDato()+" ";
     	}
     	return datosPila;
     }
     
+    private void cargarCompilador() {
+    	try {
+    		int filT=0,filR=0;
+    		String s;
+    		reglas = new String[MAX_REGLAS]; 
+    		tablaLR = new int[MAX_FIL_TABLA][MAX_COL_TABLA];    		
+			BufferedReader tabla = new BufferedReader(new FileReader(new File(RUTA_TABLA)));
+			BufferedReader regla = new BufferedReader(new FileReader(new File(RUTA_REGLAS)));
+			while((s = tabla.readLine())!=null) {
+				agregar(s.split("\t"),filT);				
+				filT++;				
+			}
+			while((s = regla.readLine())!=null) {				
+				reglas[filR] = s;
+				filR++;	
+			}
+			regla.close();
+			tabla.close();
+		} catch (IOException e) {}
+    }
     
-    private void castearTipoSimbolo(){
-    	if(col==ES_IDENTIFICADOR){
-    		col = COL_ID;
-    	}else if(col==ES_SUM_O_RES){
-    		col = COL_MAS;    		
+    private void agregar(String[] s, int fil) {
+    	int limite = s.length;
+    	for(int j=0; j<limite; j++) {
+    		tablaLR[fil][j] = Integer.valueOf(s[j]);  
     	}
     }
     
     public void pausar(){
     	try{
-    		synchronized(this){
-    			wait();
-            }
-        }catch(InterruptedException e){}           
-    }
-        
-    public void renaudar(){
-    	synchronized(this){
-    		notify();
-        }
+    		sleep(TIEMPO);    	
+    	}catch(InterruptedException e){}           
     }
 }
